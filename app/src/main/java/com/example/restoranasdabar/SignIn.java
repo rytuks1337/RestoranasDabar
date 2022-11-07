5de4267921b7fab9632e124e4124c05fd468bc15
rytuks1337 <112757266+rytuks1337@users.noreply.github.com>
2022-11-08 01:31:29 +0200

    Login and Signup (only works with Owners Database)

--- app/src/main/java/com/example/restoranasdabar/SignIn.java
+++ app/src/main/java/com/example/restoranasdabar/SignIn.java
@@ -2,13 +2,82 @@ package com.example.restoranasdabar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class SignIn extends AppCompatActivity {
 
+    EditText i_password,i_email;
+    Button signInButton, signUp_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        i_password= findViewById(R.id.edtPsw);
+        i_email= findViewById(R.id.edtEMailIn);
+        signInButton = findViewById(R.id.btnSignIn);
+        signUp_btn= findViewById(R.id.btnSignUp);
+        signInButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                String password, email;
+                password = i_password.getText().toString();
+                email = i_email.getText().toString();
+
+                if(!password.equals("")&&!email.equals("")) {
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[2];
+                            field[0] = "Email";
+                            field[1] = "Password";
+
+                            String[] data = new String[2];
+                            data[0] = email;
+                            data[1] = password;
+
+                            PutData putData = new PutData("http://192.168.56.1/databaseIn/login.php", "POST", field, data);
+
+
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+
+                                    if(result.equals("Success")){
+                                        Toast.makeText(getApplicationContext(),"Succesfull Connection",Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }else {
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(getApplicationContext(),"You need to fill all the fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        signUp_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/restoranasdabar/SignUp.java
+++ app/src/main/java/com/example/restoranasdabar/SignUp.java
@@ -2,16 +2,20 @@ package com.example.restoranasdabar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.vishnusivadas.advanced_httpurlconnection.FetchData;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class SignUp extends AppCompatActivity {
 
     EditText i_name,i_surname,i_password,i_email,i_phone_num;
+    Button signupButton, login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,32 +26,69 @@ public class SignUp extends AppCompatActivity {
         i_password= findViewById(R.id.edtPsw1);
         i_email= findViewById(R.id.edtEMail);
         i_phone_num= findViewById(R.id.edtPhone);
-        Handler handler = new Handler();
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                String[] field = new String[5];
-                field[0] = "Name";
-                field[1] = "Surname";
-                field[2] = "Email";
-                field[3] = "Password";
-                field[4] = "Phone_num";
-                //Creating array for data
-                String[] data = new String[5];
-                data[0] = i_name.getText().toString();
-                data[1] = i_surname.getText().toString();
-                data[2] = i_password.getText().toString();
-                data[3] = i_email.getText().toString();
-                data[4] = i_phone_num.getText().toString();
+        signupButton = findViewById(R.id.btnSignUp);
+        login_btn = findViewById(R.id.btnSignIn);
 
-                PutData putData = new PutData("https://projects.vishnusivadas.com/AdvancedHttpURLConnection/putDataTest.php", "POST", field, data);
-                if (putData.startPut()) {
-                    if (putData.onComplete()) {
-                        String result = putData.getResult();
-                    }
+        signupButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                String name, surname, password, email, phone;
+                name = i_name.getText().toString();
+                surname = i_surname.getText().toString();
+                password = i_password.getText().toString();
+                email = i_email.getText().toString();
+                phone = i_phone_num.getText().toString();
+                if(!name.equals("")&&!surname.equals("")&&!password.equals("")&&!email.equals("")&&!phone.equals("")) {
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[5];
+                            field[0] = "Name";
+                            field[1] = "Surname";
+                            field[2] = "Email";
+                            field[3] = "Password";
+                            field[4] = "Phone_num";
+                            //Creating array for data
+                            String[] data = new String[5];
+                            data[0] = name;
+                            data[1] = surname;
+                            data[2] = email;
+                            data[3] = password;
+                            data[4] = phone;
+
+                            PutData putData = new PutData("http://192.168.56.1/databaseIn/signup.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+
+                                    if(result.equals("Correct")){
+                                        Toast.makeText(getApplicationContext(),"Succesfull Registracion",Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(),SignIn.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }else {
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(getApplicationContext(),"You need to fill all the fields", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SignIn.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
 }
