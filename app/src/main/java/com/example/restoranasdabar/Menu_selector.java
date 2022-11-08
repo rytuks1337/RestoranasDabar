10151cf6146feda65fbdd0d9eb74ac4400017070
Svabas22 <simonas.svabas2002@gmail.com>
2022-11-09 00:35:13 +0200

    meniu selektoriaus kalendorius

--- app/src/main/java/com/example/restoranasdabar/Menu_selector.java
+++ app/src/main/java/com/example/restoranasdabar/Menu_selector.java
@@ -3,6 +3,8 @@ package com.example.restoranasdabar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class Menu_selector extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Menu_selector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_selector);
+        CalendarView calendarView=(CalendarView) findViewById(R.id.calendarView1);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), ""+dayOfMonth, Toast.LENGTH_SHORT).show();// TODO Auto-generated method stub
+
+            }
+        });
     }
 }
