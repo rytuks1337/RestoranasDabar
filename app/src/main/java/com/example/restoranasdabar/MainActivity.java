db30131890ad7b7707e1b7c57881ce01e0035061
Svabas22 <simonas.svabas2002@gmail.com>
2022-11-09 20:45:14 +0200

    Perdarytas main_activity toolbaras

--- app/src/main/java/com/example/restoranasdabar/MainActivity.java
+++ app/src/main/java/com/example/restoranasdabar/MainActivity.java
@@ -3,16 +3,43 @@ package com.example.restoranasdabar;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mNavDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar=findViewById(R.id.main_Toolbar);
+        setSupportActionBar(toolbar);
 
+        mNavDrawer=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,mNavDrawer,toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+
+        mNavDrawer.addDrawerListener(toggle);
+
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(mNavDrawer.isDrawerOpen(GravityCompat.START)){
+            mNavDrawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
